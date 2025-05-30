625f4abac565dcc873a8071f0703f96ef1893e2f
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-05-30 21:14:50 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const routes = require("./routes");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
